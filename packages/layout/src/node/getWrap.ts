4e935dde0828fb129a6e4467bba6ac24cb4c70ad
Rushabh Modi <rushabh@atharvasystem.com>
2025-12-18 15:56:06 +0530

    fix: handle edge case causing blank pages

--- packages/layout/src/node/getWrap.ts
+++ packages/layout/src/node/getWrap.ts
@@ -1,12 +1,37 @@
 import * as P from '@react-pdf/primitives';
 import { SafeNode } from '../types';
 
-const NON_WRAP_TYPES = [P.Svg, P.Note, P.Image, P.Canvas];
+export const NON_WRAP_TYPES = [P.Svg, P.Note, P.Image, P.Canvas];
 
-const getWrap = (node: SafeNode) => {
+export const canCauseBlankSpace = (node, prevNode, currentChildren) => {
+  if (!('preventBlankSpace' in node.props)) return false;
+
+  const prevNodeHasHeightOne = prevNode?.box?.height === 1;
+  const childrenIsEmpty = currentChildren?.length === 0;
+
+  // padding/margin case
+  if (prevNodeHasHeightOne === true && childrenIsEmpty === false) {
+    return true;
+  }
+
+  // gap between content case
+  if (node.box.height > 680 && childrenIsEmpty === true) {
+    return true;
+  }
+
+  return false;
+};
+
+const getWrap = (
+  node: SafeNode,
+  prevNode: SafeNode,
+  currentChildren: SafeNode[],
+) => {
   if (NON_WRAP_TYPES.includes(node.type)) return false;
 
   if (!node.props) return true;
+
+  if (canCauseBlankSpace(node, prevNode, currentChildren)) return true;
 
   return 'wrap' in node.props ? node.props.wrap : true;
 };
